3fb7bd4e2aaf9a6e23db4755baba412b2d78fe1a
Lintang Wisesa <lintangwisesa@ymail.com>
2018-02-12 14:41:27 +0700

    AWAS INI VIRUS!
    wkwk

---
+++ lin.js
@@ -0,0 +1,11 @@
+let alaskaki = 'sepatu';
+let warna = 'merah';
+
+if(alaskaki == 'sepatu' && warna == 'merah'){
+    console.log('Saya suka sepatu merah.');
+};
+
+if(alaskaki == 'sepatu' && warna == 'merah'){
+    console.log('Saya suka sepatu merah.');
+};
+
